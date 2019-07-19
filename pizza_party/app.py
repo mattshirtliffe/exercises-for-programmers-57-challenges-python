5097d799c9cfcc20a3e4d923e326bd0fbd58c958
Matthew Shirtliffe <matthew.shirtliffe@madebyspacelabs.com>
2019-07-19 18:27:35 +0100

    pizza party test print header

---
+++ pizza_party/app.py
@@ -0,0 +1,14 @@
+
+
+def print_header():
+    """ Print header 
+    """
+    header_text = ''
+    TEXT = f' Pizza Party \n'
+    line = '-' * len(TEXT)
+    line += '\n'
+
+    header_text += line
+    header_text += TEXT
+    header_text += line
+    print(header_text)
